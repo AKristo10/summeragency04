6389858abf24a796fba36411d82fb96b08b4ef4b
Andrea <andrea.kristo@fer.hr>
2021-08-06 15:20:09 +0200

    Added week-2 part-two

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -3,6 +3,7 @@ package com.agency04.sbss.pizza;
 import com.agency04.sbss.pizza.model.MarinaraPizza;
 import com.agency04.sbss.pizza.model.Pizza;
 import com.agency04.sbss.pizza.service.PizzaDeliveryService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,13 +14,11 @@ public class PizzaApp {
 
     public static void main(String[] args) {
 
-        ClassPathXmlApplicationContext context =
-                new ClassPathXmlApplicationContext("applicationContext.xml");
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(PizzaConfig.class);
 
         PizzaDeliveryService deliveryService = context.getBean("pizzaDeliveryService", PizzaDeliveryService.class);
         System.out.println(deliveryService.orderPizza(context.getBean("marinaraPizza", Pizza.class)));
-
-
 
         context.close();
 
---
+++ src/main/java/com/agency04/sbss/pizza/PizzaConfig.java
@@ -0,0 +1,36 @@
+package com.agency04.sbss.pizza;
+
+import com.agency04.sbss.pizza.model.MarinaraPizza;
+import com.agency04.sbss.pizza.model.TonnoPizza;
+import com.agency04.sbss.pizza.service.BestPizzaPizzeriaService;
+import com.agency04.sbss.pizza.service.ItalianaPizzeriaService;
+import org.springframework.context.annotation.*;
+
+@Configuration
+@ComponentScan("com.agency04.sbss.pizza")
+@PropertySource("classpath:application.properties")
+public class PizzaConfig {
+
+    @Bean
+    @Primary
+    public ItalianaPizzeriaService italianaPizzeriaService(){
+        return new ItalianaPizzeriaService();
+    }
+
+    @Bean
+    public BestPizzaPizzeriaService bestPizzaPizzeriaService(){
+        return  new BestPizzaPizzeriaService();
+    }
+
+    @Bean
+    @Primary
+    public MarinaraPizza marinaraPizza(){
+        return  new MarinaraPizza();
+    }
+
+    @Bean
+    public TonnoPizza tonnoPizza(){
+        return  new TonnoPizza();
+    }
+
+}
