386f1559413d5d29557e40bd28b48b6d169e5f37
mstuban <stuban.marko@gmail.com>
2021-08-06 19:03:54 +0200

    week-1 - add order for second pizza POJO, remove unused import

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -1,12 +1,12 @@
 package com.agency04.sbss.pizza;
 
-import com.agency04.sbss.pizza.model.MarinaraPizza;
 import com.agency04.sbss.pizza.model.Pizza;
 import com.agency04.sbss.pizza.service.PizzaDeliveryService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Main class for Pizza application
+ *
  * @author Andrea
  */
 public class PizzaApp {
@@ -19,7 +19,7 @@ public class PizzaApp {
         PizzaDeliveryService deliveryService = context.getBean("pizzaDeliveryService", PizzaDeliveryService.class);
         System.out.println(deliveryService.orderPizza(context.getBean("marinaraPizza", Pizza.class)));
 
-
+        System.out.println(deliveryService.orderPizza(context.getBean("tonnoPizza", Pizza.class)));
 
         context.close();
 
